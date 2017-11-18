a0f758921f62baf41d3388d7bbd762f8da437260
Sean Mullan <seanmullan97@outlook.com>
2017-11-18 15:41:23 +0000

    Added current for battery management system

--- bms.cpp
+++ bms.cpp
@@ -1,9 +1,12 @@
-nclude "bms.h"
+#include "bms.h"
 
 BatteryManagementSystem::BatteryManagementSystem()
 {
 }
 
 /// TASK 9 (BOTH) //////////////////////////////////////////////////////////////
-
+double BatteryManagementSystem::get_current()
+{
+  return 5;
+}
 /// END OF TASKS 9AB ///////////////////////////////////////////////////////////
